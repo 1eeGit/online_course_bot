54d111fdae235db73b2352d3ad4024c980978735
1eeGit <nakuang@student.uef.fi>
2024-09-09 01:58:40 +0300

    update structure based on ics schedule, update readme

--- courseBot.py
+++ courseBot.py
@@ -1,62 +1,71 @@
 import schedule
 import time
 import webbrowser
+import read_calendar as rc
 
 current_date = time.strftime("%Y-%m-%d", time.localtime())
 
 def open_link(self, link, specific_date = None):
     webbrowser.open(link)
 
-def join_class(link, weekday, time):
-    schedule.every().weekday.at(time).do(open_link(link))
+def join_class():
+    current_time = time.strftime("%H:%M:%S", time.localtime())
+    courses = {}
 
+    today_classes = rc.today_table()
+
+    for course in today_classes:
+        print(course)
+        course_obj = course(course.class_name, course.class_link, course.start_date, course.end_date, course.start_time, course.end_time)
+        courses[course.class_name] = course_obj
+    
+    for course in courses:
+        if current_time >= course.start_time and current_time <= course.end_time:
+            open_link(courses[course].link)
+            break
+
+### class object
+class course:
+    def __init__(self, course_name, link, start_date, end_date, start_time, end_time):
+        self.course_name = course_name
+        self.link = link
+        self.start_date = start_date
+        self.end_date = end_date
+        self.start_time = start_time
+        self.end_time = end_time 
+
+### courseBot object
 class courseBot:
     def __init__(self):
         self.schedule = schedule
-        self.start_date = None
-        self.end_date = None
+
 
     def clear_all(self):
         self.schedule.clear()
 
     def run(self):
-        while self.start_date < current_date < self.end_date:
-            self.schedule.run_pending()
+        while True:
+            join_class()
             time.sleep(1)
-        self.clear_all()
 
-    def add_check(self, link, weekday, class_time, exception_date, replacement_date):
-        weekdays = {'monday': self.schedule.every().monday, 
-                    'tuesday': self.schedule.every().tuesday, 
-                    'wednesday': self.schedule.every().wednesday, 
-                    'thursday': self.schedule.every().thursday, 
-                    'friday': self.schedule.every().friday, 
-                    'saturday': self.schedule.every().saturday, 
-                    'sunday': self.schedule.every().sunday}
+
+
+
+    # add zoom link to exsiting class
+    def add_link(self, course_name, link):
+        rc.add_link(course_name, link)
         
-        ### job is not callable error
-        self.schedule.every().tuesday.at(class_time).do(open_link, link)
-        '''
-        if weekday.lower() in weekdays:
-            if exception_date and replacement_date:
-                replacement_date_obj = time.strptime(replacement_date, "%Y-%m-%d")
-                exception_date_obj = time.strptime(exception_date, "%Y-%m-%d")
 
-                self.schedule.every().day.at(class_time).do(self.open_link, link, replacement_date_obj).tag(replacement_date)
-                weekdays[weekday.lower()]().at(class_time).do(self.open_link, link, exception_date_obj).tag(weekday)
-            else:
-                weekdays[weekday.lower()]().at(class_time).do(open_link, link)
-        else:
-            print("Invalid weekday for course on " + weekday)
-        '''
-
-    # exception and replacement date format: "%Y-%m-%d"
-    def add_class(self, link, weekday, class_time, start_date, end_date, exception_date = None, replacement_date = None):
-        self.start_date = start_date
-        self.end_date = end_date
-        if start_date < current_date < end_date:
-            self.add_check(link, weekday, class_time, start_date, end_date, exception_date, replacement_date)
-        else:
-            print("Course " + " is not ongoing")
+    
+    # TODO:
+    # add function to use the UI add and modify courses. 
+    def this_cannot_be_UI():
+        print("Welcome to use courseBot: \n \
+              Enter 1 to check today's class;\n \
+              Enter 2 to print all the classes with online meeting link;\n \
+              Enter 3 to add new course;\n \
+              Enter 4 to modify exsiting course;\n \
+              Enter q to quit.\n ")
+        
 
             
--- read_calendar.py
+++ read_calendar.py
@@ -1,6 +1,3 @@
-# export ics file, save in the same folder with the codes
-# https://studentuef.sharepoint.com/sites/PeppiHandbook/SitePages/Lukkarikone-Schedule-Assistant.aspx
-
 ############################################
 # Read ics file
 # convert into df and save in csv/ db
@@ -17,38 +14,45 @@ import pandas as pd
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
+import os
 
 
+# create engine and base globally
+Base = declarative_base()
 
-def read_ics_file(file_path, csv_name):
-    with open(file_path, "r") as f:  
+# create tables
+class Course(Base):
+    __tablename__ = "courses"
+    id = Column(Integer, primary_key=True)
+    class_name = Column(String)
+    description = Column(String)
+    class_link = Column(String)
+
+
+class timeTable(Base):
+    __tablename__ = "timetable"
+    id = Column(Integer, primary_key=True)
+    class_id = Column(Integer, ForeignKey("courses.id"))
+    start_time = Column(DateTime)
+    end_time = Column(DateTime)
+    course = relationship('Course', back_populates='timetable')
+
+Course.timetable = relationship('timeTable', order_by=timeTable.id, back_populates='course')
+
+
+# read ics file and save as txt and db file
+def read_ics_file():
+    with open("Calendar.ics", "r") as f:  
         calendar = f.read()
         
     # save raw ics in txt
-    with open(f"{csv_name}.txt", "w") as f:
+    with open("Calendar.txt", "w") as f:
         f.write(str(calendar))
 
     calendar = Calendar.from_ical(calendar)
     df = pd.DataFrame(columns=["Class_name", "Start_time", "End_time", "Description"])
 
-    engine = create_engine(f"sqlite:///{csv_name}.db")
-    Base = declarative_base()
-
-    # create table: courses, timetable
-    class Course(Base):
-        __tablename__ = "courses"
-        id = Column(Integer, primary_key=True)
-        class_name = Column(String)
-        description = Column(String)
-        class_link = Column(String)
-
-    class timeTable(Base):
-        __tablename__ = "timetable"
-        id = Column(Integer, primary_key=True)
-        class_id = Column(Integer, ForeignKey("courses.id"))
-        start_time = Column(DateTime)
-        end_time = Column(DateTime)
-
+    engine = create_engine(f"sqlite:///calendar.db")
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -62,19 +66,76 @@ def read_ics_file(file_path, csv_name):
             event_description = component.get('DESCRIPTION')
 
             new_course = Course(class_name=event_name, description=event_description, class_link="")
-            new_timeTable = timeTable(class_id=new_course.id, start_time=event_start, end_time=event_end)
-
             session.add(new_course)
-            session.add(new_timeTable)
             session.commit()
 
-            # df.loc[len(df)] = [event_name, event_start, event_end, event_description]
-    
+            new_timeTable = timeTable(class_id=new_course.id, start_time=event_start, end_time=event_end)
+            session.add(new_timeTable)
+            session.commit()
+    session.commit()
+    session.close()
 
-    # df.to_csv(f"{csv_name}.csv")
 
 
 
-# ------------------------------
-read_ics_file("calendar_week1.ics", "calendar_week1_df")
+# load db file and fetch today's classes
+# TODO:
+# Looking for Course with class_id: None
+# Warning: No course found for class_id None
+#     course_obj = course(course.class_name, course.class_link, course.start_date, course.end_date, course.start_time, course.end_time)
+# AttributeError: 'timeTable' object has no attribute 'class_name'
+def today_table():
+    db_name = "calendar"    
+    engine = create_engine(f"sqlite:///{db_name}.db")
+
+    current_date = datetime.datetime.now().date()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Fetch all classes that are scheduled for today
+    today_classes = session.query(timeTable).filter(timeTable.start_time >= current_date).all()
+
+    for class_ in today_classes:
+        print(f"Looking for Course with class_id: {class_.class_id}")
+        course = session.query(Course).filter(Course.id == class_.class_id).first()
+
+        if course:
+            print(f"Class: {course.class_name}, Start: {class_.start_time}")
+        else:
+            print(f"Warning: No course found for class_id {class_.class_id}")
+
+    session.close()
+    return today_classes
+
+
+
+# add online meeting link to exsiting class
+def add_link(class_name, link):
+    db_name = "calendar.db"
+    engine = create_engine(f"sqlite:///{db_name}")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    class_ = session.query(Course).filter(Course.class_name == class_name).first()
+    if class_:
+        class_.class_link = link
+        session.commit()
+        print(f"Link added to {class_name}: {link}")  
+    else:
+        print(f"Class {class_name} not found.") 
+
+    session.close()
+
+
+
+def db_exsiting_check():
+    if os.path.exists("calendar.db"):
+        return True
+    elif os.path.exists("Calendar.ics"):
+        print(f"Loading your ics file...")
+        read_ics_file()
+        return True
+    else:
+        print(f"Make sure you have at least save your ics file in the dir.")
+        return False
 
